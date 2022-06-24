4899be682b2e54bce01de06eac5fda811ad49fea
Dylan To <dylan.to@hotmail.com>
2022-06-25 01:09:37 +0800

    Fix wrapping for long words with no space

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,35 +2,39 @@ import React from 'react';
 import './TodoItem.css';
 
 class TodoItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: props.id,
-            completed: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.id,
+      completed: false,
+    }
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="todo-item">
+        {
+          this.state.completed ?
+            <s><li className="todo-text" onClick={this.handleDelete}>{this.props.value}</li></s> :
+            <li className="todo-text" onClick={this.handleDelete}>{this.props.value}</li>
         }
+        <input type="checkbox" checked={this.state.completed} onChange={this.toggleCompleted}/>
+      </div>
+      
+    )
+  }
 
-        this.handleDelete = this.handleDelete.bind(this);
-        this.toggleCompleted = this.toggleCompleted.bind(this);
-    }
+  handleDelete(event) {
+    this.props.onClick(this.state.id);
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <div className="todo-item">
-                {this.state.completed ? <s><li className="todo-text" onClick={this.handleDelete}>{this.props.value}</li></s> : <li className="todo-text" onClick={this.handleDelete}>{this.props.value}</li>}
-                <input type="checkbox" checked={this.state.completed} onChange={this.toggleCompleted}/>
-            </div>
-            
-        )
-    }
-
-    handleDelete(event) {
-        this.props.onClick(this.state.id);
-        event.preventDefault();
-    }
-
-    toggleCompleted() {
-        this.setState({ completed: !this.state.completed });
-    }
+  toggleCompleted() {
+    this.setState({ completed: !this.state.completed });
+  }
 }
 
 export default TodoItem;
